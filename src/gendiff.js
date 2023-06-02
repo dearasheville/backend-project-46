d015c8f7169a4bc1801c347b10e0d924675dc58d
Dear Asheville <dearasheville@outlook.com>
2023-06-02 18:39:29 +0500

    options and command-arguments added to help system

--- src/gendiff.js
+++ src/gendiff.js
@@ -3,6 +3,9 @@
 import { program } from 'commander';
 
 program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
+  .option('-f, --format <type>', 'output format')
   .parse();
