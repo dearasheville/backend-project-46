64d4043e427492d97c253a523bf0cd82f4811f73
dearasheville <dearasheville@outlook.com>
2023-06-15 21:02:07 +0500

    workflow cc configuration changes

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,22 @@
 import fs from 'fs';
+import path from 'path';
+
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import gendiff from '../src/gendiff.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 test('gendiff', () => {
-  const difference = gendiff('./__fixtures__/file1.json', './__fixtures__/file2.json');
-  const result = fs.readFileSync('./__fixtures__/result.txt', 'utf-8');
+  const filepath1 = path.join(__dirname, '../__fixtures__/file1.json');
+  const filepath2 = path.join(__dirname, '../__fixtures__/file2.json');
+
+  const filepath3 = path.join(__dirname, '../__fixtures__/result.txt');
+
+  const difference = gendiff(filepath1, filepath2);
+  const result = fs.readFileSync(filepath3, 'utf-8');
 
   console.log(result);
 
