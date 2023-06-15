321f5b2f2dde4c57aa98e491f0b26926e59a1694
dearasheville <dearasheville@outlook.com>
2023-06-15 19:08:12 +0500

    jest tests to gendiff func added

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,12 @@
+import fs from 'fs';
+
+import gendiff from '../src/gendiff.js';
+
+test('gendiff', () => {
+  const difference = gendiff('./__fixtures__/file1.json', './__fixtures__/file2.json');
+  const result = fs.readFileSync('./__fixtures__/result.txt');
+
+  console.log(result);
+
+  expect(difference).toEqual(result);
+});
