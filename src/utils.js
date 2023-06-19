8fabe83895361300656c24f791e8fbc6d6fa45ef
dearasheville <dearasheville@outlook.com>
2023-06-19 21:59:04 +0500

    module for utils added

---
+++ src/utils.js
@@ -0,0 +1,13 @@
+import path from 'path';
+
+const extensions = {
+  '.json': 'json',
+  '.yml': 'yaml',
+  '.yaml': 'yaml',
+};
+
+const getAblsolutePath = (filepath) => path.resolve(filepath);
+
+const getFileExtension = (filepath) => extensions[path.extname(filepath)];
+
+export { getAblsolutePath, getFileExtension };
